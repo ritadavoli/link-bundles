73fafe7a414a72375bfd78321b3d7c7b878e6a46
Jer Warren <jerwarren@gmail.com>
2021-09-18 14:16:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ function openLink(url, i){
     self.focus();
 }
 
+function detectPopupBlocker() {
+  var myTest = window.open("about:blank","","directories=no,height=100,width=100,menubar=no,resizable=no,scrollbars=no,status=no,titlebar=no,top=0,location=no");
+  var blocking = true;
+  if (!myTest) {
+    alert("A popup blocker was detected.");
+  } else {
+    myTest.close();
+    alert("No popup blocker was detected.");
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     openLinks();
 });
